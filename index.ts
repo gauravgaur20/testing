3b2cb8cd4b3bf849a32ed17c2063e6be87a99d98
Gaurav2496 <Gaurav@giftiicon.com>
2023-05-24 19:12:42 +0530

    inital

---
+++ index.ts
@@ -0,0 +1,14 @@
+const gaurav:string ="Hello";
+console.log("bhads");
+
+import express from 'express';
+
+const app=express();
+
+app.get('./',(req,res)=>{
+    res.send("asda");
+})
+
+app.listen(8000, ()=>{
+    console.log("listening");
+})
